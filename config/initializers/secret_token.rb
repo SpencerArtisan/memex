5ea753b73af773dcf300b3e22c218c763f261d9b
spencerkward <spencerward@hotmail.com>
2013-06-02 09:40:32 +0100

    First checkin of rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Memex::Application.config.secret_key_base = '2e7ac741416e7333302ee4d9a4695a2ba649c1e7cc00bff6b3ff7b9b0ebf595648d035f864a6cbe6291abf03fdd1c2db5fffb916450364a56103e12eb8216573'
