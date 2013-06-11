0cccad238d02c99ea4996fbabb0a52f9e178ee14
Spencer <spencerartisan@gmail.com>
2013-06-11 19:17:03 +0100

    added integration tests

--- spec/requests/memories_spec.rb
+++ spec/requests/memories_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
 describe "Memories" do
-  describe "GET /memories" do
-    it "works! (now write some real specs)" do
-      visit '/memories'
-      expect(page).to have_content 'memories'
-    end
+  it 'should allow adding of a memory' do
+    visit '/memories'
+    click_on 'New Memory'
+    fill_in 'Description', with: 'memory'
+    click_on 'Save'
+    Memory.count.should == 1
+    Memory.first.description.should == 'memory'
+  end
+
+  it 'should allow deleting of a memory' do
+    memory = Memory.create description: 'memory'
+    visit '/memories'
+    click_on 'Destroy'
+    Memory.count.should == 0
+  end
+
+  it 'should allow editing of a memory' do
+    memory = Memory.create description: 'memory'
+    visit '/memories'
+    click_on 'Edit'
+    fill_in 'Description', with: 'modified'
+    click_on 'Save'
+    Memory.count.should == 1
+    Memory.first.description.should == 'modified'
   end
 end
